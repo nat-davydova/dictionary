8ab40f195359d81da526f5a4ffcf70f3bb85345b
nat-davydova <natti.davydova@gmail.com>
2022-09-20 22:44:09 +0300

    feat: add app wrapper and eslint rules for ts import

---
+++ src/App.styles.ts
@@ -0,0 +1,5 @@
+import { styled } from "@mui/material";
+
+export const AppWrapper = styled("div")`
+  margin-top: 50px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import * as Styled from "./App.styles";
 
 function App() {
   return (
     <div className="App">
       <Container maxWidth="md">
-        <TextField label="Search a word" variant="outlined" fullWidth />
+        <Styled.AppWrapper>
+          <TextField label="Search a word" variant="outlined" fullWidth />
+          <Button variant="contained">Search</Button>
+        </Styled.AppWrapper>
       </Container>
     </div>
   );
