cf3f133d09b63e657705caf46ca6f6ae6544ffc4
nat-davydova <natti.davydova@gmail.com>
2022-09-20 22:27:39 +0300

    feat: add search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Container from "@mui/material/Container";
+import TextField from "@mui/material/TextField";
 
 function App() {
   return (
     <div className="App">
-      <Container maxWidth="md" />
+      <Container maxWidth="md">
+        <TextField label="Search a word" variant="outlined" fullWidth />
+      </Container>
     </div>
   );
 }
