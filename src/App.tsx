acf33246a48b2c6ae67cf5c5a5acb8442b28f95d
nat-davydova <natti.davydova@gmail.com>
2022-09-21 21:10:01 +0300

    feat: add basic search handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import React from "react";
 import * as Styled from "./App.styles";
 
 function App() {
+  function onSearchHandler(event: React.ChangeEvent<HTMLInputElement>): void {
+    console.log(event.target.value);
+  }
+
   return (
     <div className="App">
       <Container maxWidth="md">
         <Styled.AppWrapper>
           <Styled.SearchWrapper>
-            <TextField label="Search a word" variant="outlined" fullWidth />
+            <TextField
+              label="Search a word"
+              variant="outlined"
+              fullWidth
+              onChange={onSearchHandler}
+            />
             <Button variant="contained">Search</Button>
           </Styled.SearchWrapper>
         </Styled.AppWrapper>
