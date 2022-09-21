af3246b1cd06b276784b2301939ec75fe25d6e62
nat-davydova <natti.davydova@gmail.com>
2022-09-21 23:32:35 +0300

    feat: add a demo word loading from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import React from "react";
+import React, { useEffect } from "react";
 import * as Styled from "./App.styles";
 
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": import.meta.env.VITE_WORDS_API_KEY,
+    "X-RapidAPI-Host": "wordsapiv1.p.rapidapi.com",
+  },
+};
+
 function App() {
+  useEffect(() => {
+    fetch("https://wordsapiv1.p.rapidapi.com/words/hatchback", options)
+      .then((data) => data.json())
+      .then((data) => console.log(data));
+  }, []);
+
   function onSearchHandler(event: React.ChangeEvent<HTMLInputElement>): void {
     console.log(event.target.value);
   }
