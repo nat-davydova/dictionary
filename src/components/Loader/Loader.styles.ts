6f6aba5b3ee5790e26ae3a26e9fba64a3acde829
nat-davydova <natti.davydova@gmail.com>
2022-11-05 14:52:48 +0300

    feat: add MUI loader

---
+++ src/components/Loader/Loader.styles.ts
@@ -0,0 +1,6 @@
+import { styled } from "@mui/material";
+
+export const LoaderWrapper = styled("div")`
+  display: flex;
+  justify-content: center;
+`;
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,10 +1,11 @@
-import { Typography } from "@mui/material";
 import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import * as S from "./Loader.styles";
 
 export function Loader() {
   return (
-    <Typography variant="h5" component="p">
-      Loading...
-    </Typography>
+    <S.LoaderWrapper>
+      <CircularProgress />
+    </S.LoaderWrapper>
   );
 }
