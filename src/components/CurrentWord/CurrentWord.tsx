3a4df44cd735eaee21df9d650a62e4a6acb5944c
nat-davydova <natti.davydova@gmail.com>
2022-11-05 14:30:57 +0300

    refactor: outsource Definition into a separate component

--- src/components/CurrentWord/CurrentWord.tsx
+++ src/components/CurrentWord/CurrentWord.tsx
@@ -1,9 +1,8 @@
 import { Typography } from "@mui/material";
 import React from "react";
-import { v4 as uuid } from "uuid";
 import * as S from "./CurrentWord.styles";
 import { IWord, IWordDefinition } from "../../types";
-import { AdditionalTermsList } from "./AdditionalTermsList";
+import { Definition } from "./Definition";
 
 interface ICurrentWord {
   currentWord: IWord;
@@ -13,26 +12,7 @@ function renderWordDefinitions(definitions?: IWordDefinition[]) {
   if (!definitions) return <></>;
 
   return definitions.map((definition, index) => (
-    <S.DefinitionWrapper key={uuid()}>
-      <S.DefinitionCoreWrapper>
-        <S.DefinitionNumber>{index + 1}</S.DefinitionNumber>
-        <S.PartOfSpeech fontStyle="italic">
-          {definition.partOfSpeech}
-        </S.PartOfSpeech>
-        <S.Definition>{definition.definition}</S.Definition>
-      </S.DefinitionCoreWrapper>
-      {definition.examples?.map((example) => (
-        <S.Example key={uuid()} fontStyle="italic">
-          {example}
-        </S.Example>
-      ))}
-      {definition.synonyms?.length && (
-        <AdditionalTermsList title="Synonyms" termsList={definition.synonyms} />
-      )}
-      {definition.antonyms?.length && (
-        <AdditionalTermsList title="Antonyms" termsList={definition.antonyms} />
-      )}
-    </S.DefinitionWrapper>
+    <Definition definition={definition} number={index + 1} />
   ));
 }
 
---
+++ src/components/CurrentWord/Definition/Definition.tsx
@@ -0,0 +1,34 @@
+import { v4 as uuid } from "uuid";
+import React from "react";
+import * as S from "../CurrentWord.styles";
+import { AdditionalTermsList } from "../AdditionalTermsList";
+import { IWordDefinition } from "../../../types";
+
+interface IDefinition {
+  definition: IWordDefinition;
+  number: number;
+}
+export function Definition({ definition, number }: IDefinition) {
+  return (
+    <S.DefinitionWrapper key={uuid()}>
+      <S.DefinitionCoreWrapper>
+        <S.DefinitionNumber>{number}</S.DefinitionNumber>
+        <S.PartOfSpeech fontStyle="italic">
+          {definition.partOfSpeech}
+        </S.PartOfSpeech>
+        <S.Definition>{definition.definition}</S.Definition>
+      </S.DefinitionCoreWrapper>
+      {definition.examples?.map((example) => (
+        <S.Example key={uuid()} fontStyle="italic">
+          {example}
+        </S.Example>
+      ))}
+      {definition.synonyms?.length && (
+        <AdditionalTermsList title="Synonyms" termsList={definition.synonyms} />
+      )}
+      {definition.antonyms?.length && (
+        <AdditionalTermsList title="Antonyms" termsList={definition.antonyms} />
+      )}
+    </S.DefinitionWrapper>
+  );
+}
